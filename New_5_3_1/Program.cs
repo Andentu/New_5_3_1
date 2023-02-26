38f73fc38b2fa6aaf246a57e2d8c4b3e22922e51
Andentu <severalmaz97@gmail.com>
2023-02-26 15:44:50 +0300

    Добавил пример кода для передачи параметров по значению

--- New_5_3_1/Program.cs
+++ New_5_3_1/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            
+            // Варианты передачи параметров в методы
+            // 1. Передача по значению
+            var somename = "Евгения";
+            Console.WriteLine(somename);
+            GetName(somename);
+            Console.WriteLine(somename);
+            //При этом варианте исходные данные не изменяются никаким образом
+
+
+
+            Console.ReadKey();
+
+        }
+
+        static void GetName (string name) // Для передачи по значению
+        {
+            Console.WriteLine("Введите имя:");
+            name = Console.ReadLine();
         }
     }
 }
