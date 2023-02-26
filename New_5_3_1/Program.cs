7e2e632e5915f0b44947e957b031d2a6fc8fd039
Andentu <severalmaz97@gmail.com>
2023-02-26 15:50:24 +0300

    Добавил код для задания 5.3.1 (передача по значению)

--- New_5_3_1/Program.cs
+++ New_5_3_1/Program.cs
@@ -7,9 +7,16 @@
             // Варианты передачи параметров в методы
             // 1. Передача по значению
             var somename = "Евгения";
-            Console.WriteLine(somename);
+            Console.WriteLine("Имя " + somename);
             GetName(somename);
-            Console.WriteLine(somename);
+            Console.WriteLine("Ваше имя " + somename);
+            //При этом варианте исходные данные не изменяются никаким образом
+
+            //Задание 5.3.1 (передача по значению)
+            var someage = 15;
+            Console.WriteLine("Возраст: " + someage);
+            GetAge(someage);
+            Console.WriteLine("Ваш возраст: " + someage);
             //При этом варианте исходные данные не изменяются никаким образом
 
 
@@ -22,6 +29,12 @@
         {
             Console.WriteLine("Введите имя:");
             name = Console.ReadLine();
+        }
+        
+        static void GetAge (int age) // Для задания 5.3.1 (передача по значению)
+        {
+            Console.WriteLine("Введите ваш возраст: ");
+            age = int.Parse(Console.ReadLine());
         }
     }
 }
